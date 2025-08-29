bd23c644b4cc5d9470c1d34bcc4094d2c7ea6b19
NirmalPratheep <nirmalpratheep@gmail.com>
2025-08-29 11:15:29 -0700

    Fixed dynamic changing of nodes and edges, and changed the size of the circles in the graph visualization. Bind IP

--- app.py
+++ app.py
@@ -125,4 +125,5 @@ def compute():
     return jsonify({'graph': graph_data, 'timeline': timeline})
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+    app.run(host="0.0.0.0", port=5000, debug=True)
+
